78f8aae59716648befa2d5d37b9282f53b2efc47
Litavets <dm.stepanets@gmail,com>
2020-02-10 13:51:00 +0200

    [Final-Spring] Init project

---
+++ GoUniver-Spring/src/main/java/go/univer/GoUniverSpringApplication.java
@@ -0,0 +1,13 @@
+package go.univer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoUniverSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoUniverSpringApplication.class, args);
+	}
+
+}
---
+++ GoUniver-Spring/src/test/java/go/univer/GoUniverSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package go.univer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoUniverSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
